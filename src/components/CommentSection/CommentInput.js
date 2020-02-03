c46a86ec78e998b258dd6495b3faf2d7e1978043
Frances Hampton <bethhampton@Francess-MBP.attlocal.net>
2020-02-03 16:21:42 -0600

    Comments made now  show on screen

--- src/components/CommentSection/CommentInput.js
+++ src/components/CommentSection/CommentInput.js
@@ -1,13 +1,17 @@
 import React, { useState } from 'react';
 
 const CommentInput = props => {
+  const [newComment, setNewComment] = useState("")
+  const handleChange = (e) => {
+    setNewComment(e.target.value)
+  }
   return (
-    <form className="comment-form" onSubmit={props.submitComment}>
+    <form className="comment-form" onSubmit={e => props.submitComment(e, newComment)}>
       <input
         type="text"
-        value={props.comment}
+        value={newComment}
         placeholder="Add comment... "
-        onChange={props.changeComment}
+        onChange={e => handleChange(e)}
       />
     </form>
   );
